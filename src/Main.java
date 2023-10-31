0a2cb6a154c8273c008807a1e9aa78d148f62b21
FcoJavierVGizv <fvicgar0705@ieszaidinvergeles.org>
2023-10-31 13:57:35 +0100

    Segundo commit traduccio al castellano

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.print("Hello and welcome!");
+        System.out.print("Hello and welcome friend!");
     }
 }
